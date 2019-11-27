ad3ca7f4884fd06ebff0c43a88f08ce2c75395cc
aq_minato.naka <minato.naka@asia-quest.jp>
2019-11-27 17:19:21 +0900

    ベースのbladeとベースのルーティングを追加

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'SPA Training1') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('/build/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+
+</div>
+<!-- Scripts -->
+<script src="{{ mix('/build/js/app.js') }}" defer></script>
+</body>
+</html>
