fdedf3601d4a51e07b424306272506f85cbce2cc
aq_minato.naka <minato.naka@asia-quest.jp>
2019-11-28 15:21:15 +0900

    詳細ページ　APIでデータ取得して表示

--- resources/js/components/BookShowComponent.vue
+++ resources/js/components/BookShowComponent.vue
@@ -9,21 +9,25 @@
         <div class="row justify-content-center">
             <div class="col-sm-6">
                 <form>
-                    <div class="form-group">
-                        <label for="id">ID</label>
-                        <input type="text" class="form-control-plaintext" readonly id="id" v-bind:value="bookId">
+                    <div class="form-group row border-bottom">
+                        <label for="id" class="col-sm-3 col-form-label">ID</label>
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="id"
+                               v-model="book.id">
                     </div>
-                    <div class="form-group">
-                        <label for="title">Title</label>
-                        <input type="text" class="form-control-plaintext" readonly id="title" value="title title">
+                    <div class="form-group row border-bottom">
+                        <label for="title" class="col-sm-3 col-form-label">Title</label>
+                        <input type="text" class="col-sm-9 form-control-plaintext" readonly id="title"
+                               v-model="book.title">
                     </div>
-                    <div class="form-group">
-                        <label for="author">Author</label>
-                        <input type="text" class="form-control-plaintext" id="author" readonly value="author author">
+                    <div class="form-group row border-bottom">
+                        <label for="author" class="col-sm-3 col-form-label">Author</label>
+                        <input type="text" class="col-sm-9 form-control-plaintext" id="author" readonly
+                               v-model="book.author">
                     </div>
-                    <div class="form-group">
-                        <label for="price">Price</label>
-                        <input type="text" class="form-control-plaintext" id="price" readonly value="price price">
+                    <div class="form-group row border-bottom">
+                        <label for="price" class="col-sm-3 col-form-label">Price</label>
+                        <input type="text" class="col-sm-9 form-control-plaintext" id="price" readonly
+                               v-model="book.price">
                     </div>
                 </form>
             </div>
@@ -34,7 +38,23 @@
 <script>
     export default {
         props: {
-            bookId: String
+            bookId: Number
+        },
+        data: function () {
+            return {
+                book: {}
+            }
+        },
+        methods: {
+            getBook() {
+                axios.get('/api/books/' + this.bookId)
+                    .then((res) => {
+                        this.book = res.data;
+                    })
+            }
+        },
+        mounted() {
+            this.getBook();
         }
     }
 </script>
