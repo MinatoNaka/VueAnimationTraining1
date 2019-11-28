76ce583938ee7e53947908905584d9f188ed2286
aq_minato.naka <minato.naka@asia-quest.jp>
2019-11-28 12:55:30 +0900

    一覧取得API実装

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @return Collection
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+        return Book::all();
     }
 
     /**
